bcf9c467615b6ac7281907574aba79a1ad4743fd
LooseLi <1329307562@qq.com>
2025-07-16 17:10:49 +0800

    feat(flutter): init

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,34 @@ import 'package:flutter/material.dart';
 // 1.创建 main 函数
 // 2.在 main 函数中调用 runApp 函数
 
+// void main() {
+//   runApp(const Center(
+//     child: Text(
+//       'Hello Flutter',
+//       textDirection: TextDirection.ltr,
+//       style: TextStyle(
+//         fontSize: 24,
+//         color: Colors.white,
+//       ),
+//     ),
+//   ));
+// }
+
 void main() {
-  runApp(const Center(
-    child: Text(
-      'Hello Flutter',
-      textDirection: TextDirection.ltr,
-      style: TextStyle(
-        fontSize: 24,
-        color: Colors.white,
+  runApp(MaterialApp(
+      home: Scaffold(
+    appBar: AppBar(
+      title: const Text('导航栏标题'),
+    ),
+    body: const Center(
+      child: Text(
+        'Hello Flutter',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          fontSize: 24,
+          color: Colors.black,
+        ),
       ),
     ),
-  ));
+  )));
 }
