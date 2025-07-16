454eac1a7c119bf369eb0f5605cb07ed562daacf
LooseLi <1329307562@qq.com>
2025-07-16 17:28:10 +0800

    feat(flutter): init

--- lib/main.dart
+++ lib/main.dart
@@ -4,33 +4,41 @@ import 'package:flutter/material.dart';
 // 2.在 main 函数中调用 runApp 函数
 
 // void main() {
-//   runApp(const Center(
-//     child: Text(
-//       'Hello Flutter',
-//       textDirection: TextDirection.ltr,
-//       style: TextStyle(
-//         fontSize: 24,
-//         color: Colors.white,
-//       ),
-//     ),
-//   ));
+//   runApp(MyApp());
 // }
 
-void main() {
-  runApp(MaterialApp(
+main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-    appBar: AppBar(
-      title: const Text('导航栏标题'),
-    ),
-    body: const Center(
+        appBar: AppBar(
+          title: const Text('导航栏标题'),
+        ),
+        body: const ContentWidget(),
+      ),
+    );
+  }
+}
+
+class ContentWidget extends StatelessWidget {
+  const ContentWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
       child: Text(
         'Hello Flutter',
         textDirection: TextDirection.ltr,
         style: TextStyle(
           fontSize: 24,
-          color: Colors.black,
+          color: Colors.red,
         ),
       ),
-    ),
-  )));
+    );
+  }
 }
