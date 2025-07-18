2d828695dcbd9385b97ca56459bf17cc55b0ea5c
LooseLi <1329307562@qq.com>
2025-07-18 12:05:23 +0800

    feat(flutter): demo

--- lib/api/http_request.dart
+++ lib/api/http_request.dart
@@ -1,13 +1,14 @@
+import 'http_config.dart';
 import 'package:dio/dio.dart';
 
 class HttpRequest {
-  void request(
-    String url, {
-    String method = 'get',
-  }) async {
+  void request(String url, { String method = 'get', required Map<String, dynamic> params }) {
     // 1.创建 dio 实例
-    
-    var dio = Dio();
-    var response = await dio.get();
+    BaseOptions baseOptions = BaseOptions(
+      baseUrl: BASE_URL
+    );
+    final dio = Dio(baseOptions);
+
+    // 2.发送网络请求
   }
 }
