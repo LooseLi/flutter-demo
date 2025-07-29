09fc46d914ed012a5f929df17d0385219f79046c
LooseLi <1329307562@qq.com>
2025-07-29 16:55:30 +0800

    feat(flutter): widget

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -46,12 +46,60 @@ class _HomeBodyState extends State<HomeBody> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ListView.builder(
-          itemCount: articleList.length,
-          itemBuilder: (BuildContext context, int index) {
-            return ArticleListItem(articleList[index]);
-          }),
+    return Stack(
+      children: [
+        // 文章列表
+        Center(
+          child: ListView.builder(
+              itemCount: articleList.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ArticleListItem(articleList[index]);
+              }),
+        ),
+
+        // 底部悬浮按钮区域
+        Positioned(
+          left: 0,
+          right: 0,
+          bottom: 0,
+          child: Container(
+            height: 170, // 区域高度为170像素
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment(0.50, 0.00),
+                end: Alignment(0.50, 0.17),
+                colors: [Color(0x00F9F9EF), Color(0xFFF9F9EF)],
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // 按钮点击事件
+                    print('底部按钮被点击');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    // minimumSize: Size(200, 63), // 按钮宽度200，高度63像素
+                    backgroundColor: Theme.of(context).primaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(31.5), // 圆角效果
+                    ),
+                  ),
+                  child: Text(
+                    '开始学习',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
